c1b9bc85f1a592a37e7e31091441a114edd3ef6e
joaquinecc <joaquinecc@gmail.com>
2022-02-22 10:00:50 -0300

    fix bug auto update

--- src/table_promotion/table_promotion.js
+++ src/table_promotion/table_promotion.js
@@ -7,6 +7,7 @@
       //Listen on local storage
       window.onstorage = () => {
         // When local storage changes, update table
+        console.log("Storage value change");
         populateDataTable()
     
       };
@@ -26,30 +27,28 @@
   function populateDataTable() {
     // Do some UI setup here: change the visible section and reinitialize the table
     var data= JSON.parse(localStorage.getItem("data"))
-    const columns=Object.keys(data[0]).map(item=>   ({     "title"     :     item     }),  )
     $('#data_table_wrapper').empty();
+    if (data?.length > 0) {
+      $('#no_data_message').css('display', 'none');
+      $('#data_table_wrapper').append(`<table id='data_table' class='table table-striped table-bordered'></table>`);
 
-    if (data.length > 0) {
-        $('#no_data_message').css('display', 'none');
-        $('#data_table_wrapper').append(`<table id='data_table' class='table table-striped table-bordered'></table>`);
+      // Do some math to compute the height we want the data table to be
+      var top = $('#data_table_wrapper')[0].getBoundingClientRect().top;
+      var height = $(document).height() - top - 130;
 
-        // Do some math to compute the height we want the data table to be
-        var top = $('#data_table_wrapper')[0].getBoundingClientRect().top;
-        var height = $(document).height() - top - 130;
-
-        // Initialize our data table with what we just gathered
-        $('#data_table').DataTable({
-          "data": data.map(item=> Object.values(item)),
-          "columns":columns,
-           autoWidth: false,
-           deferRender: true,
-            scroller: true,
-            scrollY: height,
-            scrollX: true,
-            //headerCallback: headerCallback,
-            
-           // dom: "<'row'<'col-sm-6'i><'col-sm-6'f>><'row'<'col-sm-12'tr>>" // Do some custom styling
-        });
+      // Initialize our data table with what we just gathered
+      $('#data_table').DataTable({
+        "data": data.map(item=> Object.values(item)),
+        "columns":Object.keys(data[0]).map(item=>   ({     "title"     :     item     }),  ),
+          autoWidth: false,
+          deferRender: true,
+          scroller: true,
+          scrollY: height,
+          scrollX: true,
+          //headerCallback: headerCallback,
+          
+          // dom: "<'row'<'col-sm-6'i><'col-sm-6'f>><'row'<'col-sm-12'tr>>" // Do some custom styling
+      });
     } else {
         // If we didn't get any rows back, there must be no marks selected
         $('#no_data_message').css('display', 'inline');
