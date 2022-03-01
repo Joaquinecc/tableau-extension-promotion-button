007d5b36d8d0db723605d524a0a10ee055c07bee
joaquinecc <joaquinecc@gmail.com>
2022-03-01 13:50:07 -0300

    fix bug

--- src/kpi/kpi.js
+++ src/kpi/kpi.js
@@ -9,13 +9,9 @@
             // When local storage changes, update table
             console.log("Storage value change - KPI");
            // populateDataTable()
-          updateValue()
+             updateValue()
           };
-
-          updateValue()
-
-       
-    
+        updateValue()
     });
 
     function updateValue(){
@@ -34,38 +30,49 @@
           /**
          * Sum total op
          */
+        $("#num").empty()
         if(data?.length>0){
-            $("#num").empty()
             $("#num").text(numberWithPoints(Math.round(data.reduce((partialSum, a) => partialSum + a["SUMA(precio_promedio)"], 0))))
-            
+        }else{
+            $("#num").text(0)
         }
+
     }
     function countRecomendation(data){
         /**
          * Count total recomendation
          */
+         $("#num").empty()
         if(data?.length>0){
-            $("#num").empty()
             $("#num").text(numberWithPoints(data.length))
+        }else{
+            $("#num").text(0)
         }
+
     }
     function countClients(data){
         /**
          * Count distinct cod client
          */
-        if(data?.length>0){
-            $("#num").empty()
+         $("#num").empty()
+         if(data?.length>0){
             $("#num").text(numberWithPoints((new Set(data.map(item=> item.cod_cliente))).size))
+        }else{
+            $("#num").text(0)
         }
+
     }
     function countProduct(data){
         /**
          * Count distinct product
          */
+         $("#num").empty()
         if(data?.length>0){
-            $("#num").empty()
             $("#num").text(numberWithPoints((new Set(data.map(item=> item.articulocodigo))).size))
+        }else{
+            $("#num").text(0)
         }
+
     }
     function numberWithPoints(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
